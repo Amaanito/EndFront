2b7ca638ae7eb174864035e144a5f9d1c054a75d
sofianbenomar <sofianbenomar838@gmail.com>
2024-04-07 13:49:27 +0200

    Produktdata,DKpostnumre,belastningsindikatorer og fejlrapportering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css';
 import { App2} from './webShop';
 
+
 function App() {
   return (
     <div>
-      <App2 />
-      
-    
-    </div>
+      <App2/>
+
+       </div>
   );
 }
-
 export default App;
