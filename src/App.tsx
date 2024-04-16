48cd13f298c52f316af602dd1810fa4f4d6cc584
Amaanito <amaan02@live.com>
2024-04-16 15:49:11 +0200

    Route Confirm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import './App.css';
 import { App} from './webShop';
-
+import{BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Confirm from './Confirm';
 
 function RunApp() {
-  return (
-    <div>
-      <App/>
 
-       </div>
+  return (
+    <Router>
+       <div>
+        <App/>
+        <Switch>
+            <Route exact path='/'>
+            </Route>
+
+            <Route path="/confirm">
+              <Confirm/>
+
+            </Route>
+          </Switch>
+        </div>
+        
+
+    </Router>
   );
 }
 export default RunApp;
---
+++ src/Confirm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Confirm = () => {
+return(
+  <div className='confirm'>
+    <h2>
+      Negro
+    </h2>
+  </div>
+)
+
+}
+export default Confirm
