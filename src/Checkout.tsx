16aedb38bfef8c065072f208c29a0ca3fb9437d5
amaan <amaan02@live.com>
2024-04-25 21:21:02 +0200

    Tjek boksen og funktionaliteter

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Checkout = () => {
   const [termsAccepted, setTermsAccepted] = useState(false);
-  const [receiveMarketing, setReceiveMarketing] = useState(false);
   const [deliveryAddress, setDeliveryAddress] = useState({
     name: '',
     email: '',
@@ -19,6 +18,7 @@ const Checkout = () => {
   const [postnumre, setPostnumre] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -37,63 +37,61 @@ const Checkout = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    if (name === 'zipCode') {
-      const postnummerObj = postnumre.find(postnummer => postnummer.nr === value);
-      setDeliveryAddress(prevState => ({
-        ...prevState,
-        zipCode: value,
-        city: postnummerObj ? postnummerObj.navn : ''
-      }));
-    } else {
-      setDeliveryAddress(prevState => ({
-        ...prevState,
-        [name]: value
-      }));
-    }
+    setDeliveryAddress(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
+    // Validering af indtastede oplysninger
+    if (deliveryAddress.name.trim() === '' ||
+        deliveryAddress.email.trim() === '' ||
+        deliveryAddress.phone.trim() === '' ||
+        deliveryAddress.addressLine1.trim() === '' ||
+        deliveryAddress.zipCode.trim() === '' ||
+        deliveryAddress.city.trim() === '' ||
+        !termsAccepted) {
+      alert('Accepter vilkår og betingelser for at fortsætte.');
+      return;
+    }
+    // Håndtering af formularsubmit
+    setFormSubmitted(true);
   };
 
   return (
     <div>
-      <div style={{ margin: -50 }}></div>
-      <div>
-        <h1>Leverings- og faktureringsadresse</h1>
-      </div>
+      <h1>Leverings- og faktureringsadresse</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <input type="text" name="name" placeholder="Navn" required value={deliveryAddress.name} onChange={handleInputChange} 
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
-          <input type="email" name="email" placeholder="Email" required value={deliveryAddress.email} onChange={handleInputChange}  
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          <input type="email" name="email" placeholder="Email" required value={deliveryAddress.email} onChange={handleInputChange} 
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
           <input type="text" name="phone" placeholder="Telefon" required value={deliveryAddress.phone} onChange={handleInputChange} 
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
           <input type="text" name="addressLine1" placeholder="Adresse linje 1" required value={deliveryAddress.addressLine1} onChange={handleInputChange} 
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
           <input type="text" name="addressLine2" placeholder="Adresse linje 2" value={deliveryAddress.addressLine2} onChange={handleInputChange} 
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
-          <label htmlFor="zipCode"></label>
           <select
-            id="zipCode"
             name="zipCode"
             required
             value={deliveryAddress.zipCode}
             onChange={handleInputChange}
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }}
-          >
+            style={{ width: '315px', height: '25px', marginBottom: '10px' }} 
+            >
             <option value="">Vælg postnummer</option>
             {postnumre.map(postnummer => (
               <option key={postnummer.nr} value={postnummer.nr}>
@@ -104,47 +102,49 @@ const Checkout = () => {
           {isLoading && <p>Postnumre indlæses...</p>}
           {error && <p>{error}</p>}
         </div>
+        
         <div>
-          <input type="text" name="city" placeholder="By" required value={deliveryAddress.city} onChange={handleInputChange}
-            style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
+          <input type="text" name="city" placeholder="By" required value={deliveryAddress.city} onChange={handleInputChange} 
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
-          <input type="text" name="country" placeholder="Land" required value={deliveryAddress.country} onChange={handleInputChange} disabled
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          <input type="text" name="country" placeholder="Land" required value={deliveryAddress.country} onChange={handleInputChange} disabled 
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
           <input type="text" name="companyName" placeholder="Firmanavn" value={deliveryAddress.companyName} onChange={handleInputChange}
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
         <div>
-          <input type="text" name="vatNumber" placeholder="CVR-nummer" value={deliveryAddress.vatNumber} onChange={handleInputChange}
-            style={{ width: '300px', height: '20px', marginBottom: '10px' }} />
+          <input type="text" name="vatNumber" placeholder="CVR-nummer" value={deliveryAddress.vatNumber} onChange={handleInputChange} 
+          style={{ width: '305px', height: '20px', marginBottom: '10px' }} />
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '10px' }}>
-          <label htmlFor="orderComment" style={{ alignSelf: 'center', marginBottom: '5px' }}>Kommentar til ordre:</label>
+        <div style={{ marginBottom: '10px' }}>
+          <label htmlFor="orderComment"></label>
           <textarea
             id="orderComment"
             name="orderComment"
             placeholder="Tilføj en kommentar til din ordre her..."
-            style={{ width: '50%', height: '100px' }}
+            style={{ width: '100%', height: '150px', padding: '10px', boxSizing: 'border-box' }}
           ></textarea>
         </div>
-        <div style={{ textAlign: 'center' }}>
-          <Link to="/">
-            <button style={{ backgroundColor: 'red', color: 'white', width: '125px', marginTop: '10px' }}>Tilbage</button>
-          </Link>
-          <Link to="/payment">
-            <button type="submit" style={{ backgroundColor: 'green', color: 'white', width: '125px' }}>Fortsæt</button>
-          </Link>
-        </div>
-        <div>
+        
+        
+         <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+          <div style={{ marginRight: '15px' }}>
+            <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+              <button style={{ backgroundColor: 'red', color: 'white', width: '125px' }}>Tilbage</button>
+              </Link>
+              </div>
+               <div style={{ marginLeft: '10px' }}>
+                <button type="submit" style={{ backgroundColor: 'green', color: 'white', width: '125px' }}>Bekræft</button>
+                </div>
+            </div>
+           <div>
           <input type="checkbox" id="terms" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} />
           <label htmlFor="terms">Jeg accepterer vilkår og betingelser</label>
         </div>
-        <div>
-          <input type="checkbox" id="marketing" checked={receiveMarketing} onChange={(e) => setReceiveMarketing(e.target.checked)} />
-          <label htmlFor="marketing">Jeg ønsker at modtage marketingemails</label>
-        </div>
+        {formSubmitted && <Link to="/payment">Fortsæt til betaling →</Link>}
       </form>
     </div>
   );
