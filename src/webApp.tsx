f3b97fb32a6b4ccb6ab4a5067d968d669de41191
Amaanito <amaan02@live.com>
2024-04-29 16:14:43 +0200

    Håndtering af reverts

--- src/webApp.tsx
+++ src/webApp.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import { Helmet } from "react-helmet";
 export function ProductList({ products, addToCart, upsellNotification }) {
   const numProductsPerRow = 4;
-  const columnWidth = `calc(100% / ${numProductsPerRow} - 20px)`;
-  const marginRight = "20px";
+  const columnWidth = `calc(100% / ${numProductsPerRow} - 15px)`; // Reducerer marginen
+  const marginRight = "8px"; // Ændret margin
   const productHeight = "475px";
-
   return (
     <div
       style={{
         display: "flex",
         flexWrap: "wrap",
-        justifyContent: "space-between",
+        justifyContent: "center", // Ændret justifyContent til "center"
         alignItems: "stretch",
       }}
     >
@@ -44,7 +43,6 @@ export function ProductList({ products, addToCart, upsellNotification }) {
                 <div style={{ fontWeight: "bold", marginBottom: "10px" }}>
                   {product.name}
                 </div>
-
                 <img
                   src={product.imageUrl}
                   alt={product.name}
@@ -54,10 +52,8 @@ export function ProductList({ products, addToCart, upsellNotification }) {
                     height: "300px",
                   }}
                 />
-
                 <div style={{ marginBottom: "10px" }}>{product.price} DKK</div>
               </div>
-
               <div>
                 <button
                   style={{ marginTop: "10px", backgroundColor: "orange" }}
@@ -65,7 +61,6 @@ export function ProductList({ products, addToCart, upsellNotification }) {
                 >
                   Tilføj til kurv
                 </button>
-
                 {product.upsellProductId && (
                   <button
                     style={{
@@ -83,23 +78,20 @@ export function ProductList({ products, addToCart, upsellNotification }) {
           </div>
         </div>
       ))}
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
     </div>
   );
 }
-
 export function CartItem({ item, removeFromCart, updateQuantity }) {
   // Beregner totalen for hver vare baseret på pris og antal
-  const itemTotal = item.price * item.quantity;
-  
   // Beregner varebaseret rabat hvis betingelserne er opfyldt
   let itemDiscount = 0;
   if (item.quantity >= item.rebateQuantity && item.rebateQuantity > 0) {
     itemDiscount = item.price * item.quantity * (item.rebatePercent / 100);
   }
-
   // Beregner den endelige total for varen efter rabat
-  const itemFinalTotal = itemTotal - itemDiscount;
-
   return (
     <div className="cart-item">
       <img src={item.imageUrl} alt={item.name} className="cart-item-image" />
@@ -108,51 +100,64 @@ export function CartItem({ item, removeFromCart, updateQuantity }) {
         <p className="cart-item-price">Pris: {item.price.toFixed(2)} kr.</p>
         <div className="cart-item-quantity">
           Antal:
-          <select 
-            className="quantity-select" 
-            value={item.quantity} 
-            onChange={(e) => updateQuantity(item.id, parseInt(e.target.value, 10))}
+          <select
+            className="quantity-select"
+            value={item.quantity}
+            onChange={(e) =>
+              updateQuantity(item.id, parseInt(e.target.value, 10))
+            }
           >
-            {[...Array(10).keys()].map(i => (
-              <option key={i + 1} value={i + 1}>{i + 1}</option>
+            {[...Array(10).keys()].map((i) => (
+              <option key={i + 1} value={i + 1}>
+                {i + 1}
+              </option>
             ))}
           </select>
         </div>
         {itemDiscount > 0 && (
-          <p className="cart-item-discount">Rabat: -{itemDiscount.toFixed(2)} kr.</p>
+          <p className="cart-item-discount">
+            Rabat: -{itemDiscount.toFixed(2)} kr.
+          </p>
         )}
-        <p className="cart-item-final-total">Total: {itemFinalTotal.toFixed(2)} kr.</p>
-        <button 
-          className="remove-button" 
+
+ <ul style={{ marginTop:"15px" }}></ul>
+        <button
+
+          className="remove-button"
           onClick={() => removeFromCart(item.id)}
         >
+
+
+       <ul style={{ marginLeft:"10px" }}></ul>
           Fjern
         </button>
+
+
+
       </div>
+
     </div>
   );
 }
-
-
 export function ShoppingCart({ cart, removeFromCart, updateQuantity }) {
   // Beregner subtotalen for indkøbskurven uden ordrebaseret rabat
-  const subtotal = cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-
+  const subtotal = cart.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
   // Beregner den samlede rabat for varerne
   const totalItemDiscounts = cart.reduce((acc, item) => {
     const itemTotal = item.price * item.quantity;
-    const itemDiscount = item.quantity >= item.rebateQuantity && item.rebateQuantity > 0
-      ? itemTotal * (item.rebatePercent / 100)
-      : 0;
+    const itemDiscount =
+      item.quantity >= item.rebateQuantity && item.rebateQuantity > 0
+        ? itemTotal * (item.rebatePercent / 100)
+        : 0;
     return acc + itemDiscount;
   }, 0);
-
   // Tjekker om subtotalen kvalificerer til en ordrebaseret rabat
-  const orderDiscount = subtotal > 300 ? subtotal * 0.1 : 0;
-
+  const orderDiscount =
+    subtotal > 300 ? (subtotal - totalItemDiscounts) * 0.1 : 0;
   // Beregner den totale pris med alle rabatter
-  const total = subtotal - totalItemDiscounts - orderDiscount;
-
   return (
     <div>
       {cart.length === 0 ? (
@@ -169,53 +174,61 @@ export function ShoppingCart({ cart, removeFromCart, updateQuantity }) {
           ))}
           <li className="cart-summary">
             {totalItemDiscounts > 0 && (
-              <p>Total varebaseret rabat: -{totalItemDiscounts.toFixed(2)} DKK</p>
+              <p>
+                Total varebaseret rabat: -{totalItemDiscounts.toFixed(2)} DKK
+              </p>
             )}
             {orderDiscount > 0 && (
               <p>Ordrebaseret rabat: -{orderDiscount.toFixed(2)} DKK</p>
             )}
             <p>Subtotal: {subtotal.toFixed(2)} DKK</p>
-            <p className="order-total">Total: {total.toFixed(2)} DKK</p>
           </li>
-
         </ul>
       )}
     </div>
   );
 }
-
-
-
 export function App2() {
   const [products, setProducts] = React.useState([]);
   const [cart, setCart] = React.useState([]);
   const [totalPrice, setTotalPrice] = React.useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isCartEmpty, setIsCartEmpty] = useState(true);
-
   useEffect(() => {
-    setIsLoading(true);
-    fetch("/items.json")
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("/items.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
         setProducts(data);
         setIsLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error fetching data:", error);
         setIsLoading(false);
-      });
+      }
+    };
+    fetchData();
   }, []);
-
   useEffect(() => {
     setIsCartEmpty(cart.length === 0);
   }, [cart]);
-
+  useEffect(() => {
+    const metaViewport = document.createElement("meta");
+    metaViewport.setAttribute("name", "viewport");
+    metaViewport.setAttribute("content", "width=device-width, initial-scale=1");
+    document.head.appendChild(metaViewport);
+  
+    return () => {
+      document.head.removeChild(metaViewport);
+    };
+  }, []);
   const addToCart = (productId) => {
     const productToAdd = products.find((product) => product.id === productId);
     const existingCartItem = cart.find((item) => item.id === productId);
     let newCart;
-
     if (existingCartItem) {
       newCart = cart.map((item) =>
         item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
@@ -223,11 +236,9 @@ export function App2() {
     } else {
       newCart = [...cart, { ...productToAdd, quantity: 1 }];
     }
-
     setCart(newCart);
     setTotalPrice(calculateTotalPrice(newCart));
   };
-
   const removeFromCart = (productId) => {
     const newCart = cart.reduce((acc, item) => {
       if (item.id === productId) {
@@ -239,20 +250,19 @@ export function App2() {
       }
       return acc;
     }, []);
-
     setCart(newCart);
     setTotalPrice(calculateTotalPrice(newCart));
   };
-
   const updateQuantity = (productId, quantity) => {
-    setCart(prevCart => {
-      return prevCart.map(item =>
+    setCart((prevCart) => {
+      const updatedCart = prevCart.map((item) =>
         item.id === productId ? { ...item, quantity } : item
       );
+      const totalPrice = calculateTotalPrice(updatedCart);
+      setTotalPrice(totalPrice);
+      return updatedCart;
     });
   };
-
-
   const calculateTotalPrice = (cart) => {
     let totalPrice = 0;
     cart.forEach((item) => {
@@ -262,14 +272,11 @@ export function App2() {
       }
       totalPrice += itemTotal;
     });
-
     if (totalPrice > 300) {
       totalPrice *= 0.9;
     }
-
     return totalPrice;
   };
-
   const upsellNotification = (upsellProductId) => {
     const upsellProduct = products.find(
       (product) => product.id === upsellProductId
@@ -278,16 +285,14 @@ export function App2() {
       `Overvej også ${upsellProduct.name} til ${upsellProduct.price} DKK for en bedre værdi!`
     );
   };
-
   const scrollToShoppingCart = () => {
     document
       .getElementById("shopping-cart")
       .scrollIntoView({ behavior: "smooth", block: "start" });
   };
-
   return (
     <div style={{ margin: -40 }}>
-       {isLoading && <div data-testid="loading-indicator">Loading...</div>}
+      {isLoading && <div data-testid="loading-indicator">Indlæser...</div>}
       <h1
         style={{
           maxWidth: "100%",
@@ -309,14 +314,12 @@ export function App2() {
           onClick={scrollToShoppingCart}
         />
       </h1>
-
       <h1>Produkter</h1>
       <ProductList
         products={products}
         addToCart={addToCart}
         upsellNotification={upsellNotification}
       />
-
       <img
         src="kurv.png"
         style={{
@@ -326,53 +329,49 @@ export function App2() {
           background: "none",
         }}
       />
-
       <h1 id="shopping-cart">Indkøbsvogn</h1>
-
-      <ShoppingCart cart={cart} removeFromCart={removeFromCart}
-        // Tilføj denne prop
-        updateQuantity={updateQuantity} />
-      <h3>
-      </h3>
-
+      <ShoppingCart
+        cart={cart}
+        removeFromCart={removeFromCart}
+        updateQuantity={updateQuantity}
+      />
+      <h3>Total pris: {totalPrice.toFixed(2)} DKK</h3>
       <div
-
-
-
-  style={{
-    textAlign: "right",
-    marginRight: "150px",
-    marginTop: "80px",
-    marginBottom: "80px",
-  }}
->
-  <Link
-    to={!isCartEmpty ? {
-      pathname: "/checkout",
-      state: { productsInCart: cart }, // Overfør kurvens indhold som en del af state-objektet
-    } : null}
-  >
-    <button
-      style={{
-        backgroundColor: isCartEmpty ? "#ccc" : "green",
-        color: "white",
-        width: "125px",
-      }}
-      onClick={() => {
-        if (isCartEmpty) {
-          alert("Du skal vælge mindst én vare, før du kan gå til kassen.");
-        }
-      }}
-    >
-      Gå til kassen
-    </button>
-  </Link>
-</div>
-
-
-
-
-
+        style={{
+          textAlign: "right",
+          marginRight: "150px",
+          marginTop: "80px",
+          marginBottom: "80px",
+        }}
+      >
+        <Link
+          to={
+            !isCartEmpty
+              ? {
+                  pathname: "/checkout",
+                  state: { productsInCart: cart },
+                }
+              : ""
+          }
+        >
+          <button
+            style={{
+              backgroundColor: isCartEmpty ? "#ccc" : "green",
+              color: "white",
+              width: "125px",
+            }}
+            onClick={() => {
+              if (isCartEmpty) {
+                alert(
+                  "Du skal vælge mindst én vare, før du kan gå til kassen."
+                );
+              }
+            }}
+          >
+            Gå til kassen
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
