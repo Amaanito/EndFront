fb6d69f66866a900fa294b8fef948a83966ddb80
Ali Masoud <96056143+AliTheBoss03@users.noreply.github.com>
2024-05-03 00:44:36 +0200

    ip address configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Angiv den ønskede IP-adresse
+    port: 3000, // Angiv den ønskede port
+  },
 })
